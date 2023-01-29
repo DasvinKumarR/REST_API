e7363958ebf691faba8eeec93676afb4659e4c3a
DasvinKumarR <dasvinkumar2404@gmail.com>
2023-01-29 16:10:27 +0530

    Week2 Assignment Updates done with dataset file

--- JiraCollection/bat_file/JiraCurdBat.bat
+++
@@ -1,3 +0,0 @@
-cd D:\postman Assignment\REST_API\JiraCollection
-
-newman run JiraCollectionCurdOperation.postman_collection.json -e JiraHost.postman_environment.json -g "My Workspace.postman_globals.json" -r htmlextra
