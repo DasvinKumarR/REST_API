2e9cf4649dbd03824b2e7fb35db7239be25e99db
DasvinKumarR <dasvinkumar2404@gmail.com>
2023-02-05 12:24:49 +0530

    This Jira Collection Assignment Please check this assignment in zipped file

---
+++ JiraCollection/bat_file/JiraCollectionBat.bat
@@ -0,0 +1,2 @@
+cd D:\postman Assignment\REST_API\JiraCollection
+newman run JiraCollectionCurdOperation.postman_collection.json -e JiraHost.postman_environment.json -g "My Workspace.postman_globals.json" -d JiraApiDD.csv -r htmlextra
